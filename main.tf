25a7efaf6a4ddd0a3502df397765a79204a1bc55
rickymaw <rickym@aplaceformom.com>
2020-06-19 15:46:19 -0700

    Initial terraform resource definitions and action.yaml

---
+++ main.tf
@@ -0,0 +1,36 @@
+resource "aws_s3_bucket" "bucket" {
+  bucket = var.bucket_name
+  acl = var.acl
+  policy = var.bucket_policy
+  region = var.region
+
+  tags = {
+    app   = var.project_name
+    owner = var.project_owner
+    env   = terraform.workspace
+    repo  = var.github_repository
+  }
+}
+
+resouce "aws_iam_policy" "policy" {
+  name = "s3-${aws_s3_bucket.id}-RW-Access"
+  description = "RW Access to S3 bucket. ${var.github_repository}"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:*"
+      ],
+      "Effect": "Allow",
+      "Resource": [
+        "${aws_s3_bucket.bucket.arn}",
+        "${aws_s3_bucket.bucket.arn}/*"
+      ]
+    }
+  ]
+}
+POLICY
+}
