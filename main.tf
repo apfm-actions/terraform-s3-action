5f5a8ed06338e50674174d2cd8b44ec51aeacd37
rickymaw <rickym@aplaceformom.com>
2020-06-25 15:21:51 -0700

    Implemented S3 website feature

--- main.tf
+++ main.tf
@@ -1,8 +1,18 @@
 resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
-  acl = var.acl
+  acl    = var.acl
   policy = var.bucket_policy
   region = var.region
+
+  dynamic "website" {
+    for_each = var.enable_website ? [] : ["1"]
+
+    content {
+      index_document           = var.website_index_document
+      error_document           = var.website_error_document
+      redirect_all_requests_to = var.website_redirect
+    }
+  }
 
   tags = {
     app   = var.project_name
@@ -13,7 +23,7 @@ resource "aws_s3_bucket" "bucket" {
 }
 
 resource "aws_iam_policy" "policy" {
-  name = "s3-${aws_s3_bucket.bucket.id}-RW-Access"
+  name        = "s3-${aws_s3_bucket.bucket.id}-RW-Access"
   description = "RW Access to S3 bucket. ${var.github_repository}"
 
   policy = <<POLICY
